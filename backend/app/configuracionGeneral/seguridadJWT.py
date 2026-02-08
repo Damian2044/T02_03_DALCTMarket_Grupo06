72acfcffd8ac0d9e98f0f71921d1e5acd3df2efb
Damian2044 <dambarguz.2004@gmail.com>
2026-02-08 00:03:52 -0500

    Actualización de permisos para actualizar contraseña

--- backend/app/configuracionGeneral/seguridadJWT.py
+++ backend/app/configuracionGeneral/seguridadJWT.py
@@ -41,7 +41,7 @@ oauth2Scheme = OAuth2PasswordBearer(tokenUrl="/usuarios/loginSwagger",auto_error
 #Validador de token y permisos según el rol
 permisos = {
     "Prueba": {"Administrador": "ALL", "Bodeguero": "ALL", "Cajero": "ALL"},
-    "ParametrosSistema": {"Administrador": "ALL", "Bodeguero": [], "Cajero": []},
+    "ParametrosSistema": {"Administrador": "ALL", "Bodeguero": ["POST"], "Cajero": ["POST"]},
     "Usuarios": {"Administrador": "ALL", "Bodeguero": [], "Cajero": []},
     "Productos": {"Administrador": "ALL", "Bodeguero": "ALL", "Cajero": ["GET"]},
     "Inventario": {"Administrador": ["ALL","GET","POST","PUT","DELETE"], "Bodeguero": ["GET","POST","PUT"], "Cajero": ["GET"]},
